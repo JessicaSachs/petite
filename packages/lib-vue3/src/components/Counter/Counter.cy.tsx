bfd993d74120a49b8aacb0e2dc1694acdb2297d7
Jess <jess@jessicasachs.io>
2023-05-11 10:33:34 +0100

    Update Counter.cy.tsx

--- packages/lib-vue3/src/components/Counter/Counter.cy.tsx
+++ packages/lib-vue3/src/components/Counter/Counter.cy.tsx
@@ -2,8 +2,6 @@ import Counter from './Counter.vue'
 
 describe('<Counter />', () => {
   it('renders', () => {
-    // see: https://test-utils.vuejs.org/guide/
-
     cy.mount(() => <Counter max={10} min={-1}></Counter>).get('button').first().click().click()
   })
 })
